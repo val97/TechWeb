e30de019a4834063dcf496680ded0afae52cccbd
val97 <valentina.ferraioli@studio.unibo.it>
2018-07-23 17:57:59 +0200

    Add files via upload

--- script1.js
+++ script1.js
@@ -9,6 +9,8 @@
 
       // 3. This function creates an <iframe> (and YouTube player)
       //    after the API code downloads.
+
+
       var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
@@ -36,45 +38,98 @@
           setTimeout(stopVideo, 6000);
           done = true;
         }
-      }
+      /*  if (event.data == YT.PlayerState.PAUSED ) {
+          player.loadVideoById("bHQqvYy5KYo", 5, "large");
+      }*/
+    }
       function stopVideo() {
         player.stopVideo();
       }
 
+
+
       //viene riempita la lista delle canzoni iniziali
       function grafica(data){
           for (var i = 0; i < data.length; i++) {
-            $("#main").append("<button>"+data[i].category+ data[i].artist+data[i].title+data[i].videoID+"</button>");
+             $("#main").append("<button data-dismiss='modal' onclick='caricavideo("+JSON.stringify(data[i].videoID)+")'>"+data[i].category+ data[i].artist+data[i].title+data[i].videoID+"</button>");
           }
         }
+
+        function caricavideo(data){
+          // console.log(JSON.stringify(data));
+           player.loadVideoById(data, 0, "large");
+           setTimeout(salvarecent,20000,data);
+
+        }
+
+        var recommender_size=20;    //dimensione tabella degli ultimi video visitati
+        var shift=10;   //siccome la chiave 0 (=null) é melgio non usarla, viene sciftato tutto di shift (non é importante quanto valga)
+        function salvarecent(ID){
+          var key=localStorage.getItem("counter");  //counter=valore della prossima chiave libera 
+          if (key==null)    //se non esiste già
+            key=shift;  
+          localStorage.setItem(key, ID);
+          key++;
+          key=(key-shift)%recommender_size+shift;
+          localStorage.setItem("counter", key);
+
+          console.log(ID);
+        }
+        function caricarecent(out){
+          var i;
+          for(i=0;i<recommender_size;i++){
+          key=(localStorage.getItem("counter")-shift-i-1+recommender_size)%recommender_size+shift;
+          var video=localStorage.getItem(key);
+          if (video==null)
+            return i;   //dimensione di out
+
+          out[i]= video;     //returna null se l'eselmento non esiste
+          }
+          return recommender_size;
+        }
+
+
         function riempi(){
           $.ajax({
 
             url: "http://site1825.tw.cs.unibo.it/video.json",
             success: function(data) {
+
               grafica(data);
-              //$("#main").append("ciao"+ data[i].category);
-
-
+             
             },
             error: function(data) {
               alert("Caricamento impossibile");
             }
           });       
         }
+       function stampa(vid, dim){
+        var html="";
+          for (var i = 0; i < dim; i++) {
+            html+="<img width=7% heigth=7% src ='https://img.youtube.com/vi/"+vid[i]+"/0.jpg'>";
+          }
+          $(".tabcontent").html(html);
+        }
 
-        function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-    
+        function caricaTab(evt, category) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(category).style.display = "block";
+            evt.currentTarget.className += " active";
+
+            var dim=recommender_size;
+            var vid=[recommender_size];
+            if(category=="Recent")
+              dim=caricarecent(vid);
+            stampa(vid,dim);
+        }
+        
+
 
