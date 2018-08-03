50a2df23604094c95f61f876c22144405ea9b281
val97 <valentina.ferraioli@studio.unibo.it>
2018-08-03 16:25:57 +0200

    Add files via upload
    il server mostra il sito funzionante

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('./search_luc.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-}).listen(8080); 
+
+
+var fs = require("fs");
+var http = require("http");
+var url = require("url");			//permette di caricare anche gli script del file html
+
+http.createServer(function (request, response) {
+
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    response.writeHead(200);
+
+    if(pathname == "/") {
+        html = fs.readFileSync("progetto.html", "utf8");
+        response.write(html);
+    } else if (pathname == "/script1.js") {
+        script = fs.readFileSync("script1.js", "utf8");
+        response.write(script);
+    }
+
+
+    response.end();
+}).listen(8000);
+
