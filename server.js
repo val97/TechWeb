8fcdd7177e056b386e54fe3e4f603c4b885fac6b
fruscello <34569702+fruscello@users.noreply.github.com>
2018-08-29 17:55:24 +0200

    Update server.js
    per far funzionare popularity

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-
-
 var fs = require("fs");
 var http = require("http");
+var locpop = require ('./popularity0.4.js');
 var url = require("url");			//permette di caricare anche gli script del file html
 
 http.createServer(function (request, response) {
@@ -18,8 +17,44 @@ http.createServer(function (request, response) {
         script = fs.readFileSync("script1.js", "utf8");
         response.write(script);
     }
-
-
-    response.end();
+    if(pathname=="/popularity"){
+	var q = url.parse(request.url, true).query;
+	var from_id = q.from_id;
+	var to_id = q.to_id;
+	var reason=q.reason;
+	if(reason!=null){
+	
+		var prom;
+		if(reason=="popLocAss"){
+			prom=locpop.findLocAss();
+		}
+		if(reason=="popLocRel"){
+			console.log("loc rel!!!");
+			prom=locpop.findLocRel(from_id);
+		}
+		if(reason=="popGlobAss"){
+			prom=locpop.findGlobAss();
+		}
+		if(reason=="popGlobRel"){
+			prom=locpop.findGlobRel(from_id);
+		}
+		prom.then(function(data){
+			response.end(JSON.stringify(data));
+		});
+	}else{
+		if(from_id != null){
+			if(to_id == null){
+				locpop.insertLocAss(from_id);
+				console.log("inserito: "+from_id);
+			}else{
+				locpop.insertLocRel(from_id,to_id);
+				console.log("inserito: "+to_id+" dopo "+from_id);
+			}
+		}
+		response.end();
+	}
+    }else{
+    	response.end();
+    }
+	//response.end();
 }).listen(8000);
-
