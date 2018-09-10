d0ffe4fa8bb4b9976305c94979f5d5c01118e252
val97 <valentina.ferraioli@studio.unibo.it>
2018-09-10 14:11:43 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -136,9 +136,10 @@
 			           var artistInfo="";
                                    wiki=data.results.bindings[0].Sabstract;
 				   artistInfo=data.results.bindings[0].abstract; //se si vogliono aggiungere le info del cantant
-                                    if(data.results.bindings[0].Sabstract)
+                                    if(data.results.bindings[0].Sabstract){
                                       $('#wiki_container').html(wiki.value);
 				      $('#artist_container').html(artistInfo.value);
+				    }
                                     else
                                       $('#wiki_container').html("no abs founded\n");
                                 });
